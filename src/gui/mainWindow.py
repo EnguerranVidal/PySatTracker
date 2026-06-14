3cc47a8c9aa04691c2500e1392795dc6818c643e
Enguerran <engue11@hotmail.com>
2026-06-14 21:18:26 +0200

    Add Scatter plot type and UI integration
    Introduce a new Scatter plot implementation and wire it into the UI. Added src/gui/plots/scatter.py which defines ScatterPlot (subclassing LinePlot) plus ScatterPlotSettingsWidget and ScatterSettingsPage; the plot creates separate X/Y data requests and exposes configuration for markers. mainWindow.py updated to import ScatterPlot, add QAction, toolbar/menu entry, icon key, addScatterPlot/_addScatterPlot handlers, and to restore saved SCATTER dock widgets. src/gui/plots/general.py updated to recognize SCATTER plot type and show the scatter settings tab. This change enables creating, configuring and persisting scatter plots in the application.

--- src/gui/mainWindow.py
+++ src/gui/mainWindow.py
@@ -14,6 +14,7 @@ from src.gui.plots.requests import PlotRequestRegistryWindow, PlotRequestManager
 from src.gui.plots.line import LinePlot
 from src.gui.plots.time import TimeSeriesPlot
 from src.gui.plots.polar import PolarPlot
+from src.gui.plots.scatter import ScatterPlot
 from src.gui.common import TimelineWidget, SimulationClock, OrbitWorker, SetTimeDialog
 from src.gui.view3d.general import View3dWidget
 from src.gui.utilities import generateDefaultSettingsJson, loadSettingsJson, saveSettingsJson, getKeyFromValue
@@ -252,6 +253,11 @@ class MainWindow(QMainWindow):
         self.addPolarPlotAction.setIcon(self.icons['POLAR'])
         self.addPolarPlotAction.setStatusTip('Add a Polar Plot to the Current Plot Tab')
         self.addPolarPlotAction.triggered.connect(self._addPolarPlot)
+        # ADD SCATTER PLOT
+        self.addScatterPlotAction = QAction('&Add Scatter Plot', self)
+        self.addScatterPlotAction.setIcon(self.icons['SCATTER'])
+        self.addScatterPlotAction.setStatusTip('Add a Scatter Plot to the Current Plot Tab')
+        self.addScatterPlotAction.triggered.connect(self._addScatterPlot)
 
         # OPEN DATA ENGINE REQUESTS REGISTRY
         self.openEngineRegistryAction = QAction('&Data Registry', self)
@@ -317,6 +323,7 @@ class MainWindow(QMainWindow):
         self.plotMenu.addAction(self.addTimeSeriesAction)
         self.plotMenu.addAction(self.addLinePlotAction)
         self.plotMenu.addAction(self.addPolarPlotAction)
+        self.plotMenu.addAction(self.addScatterPlotAction)
         ### TOOLS MENU ###
         self.toolsMenu = self.menuBar.addMenu('&Tools')
         self.simulationMenu = self.toolsMenu.addMenu('&Simulation')
@@ -359,6 +366,7 @@ class MainWindow(QMainWindow):
         self.plotViewToolBar.addAction(self.addTimeSeriesAction)
         self.plotViewToolBar.addAction(self.addLinePlotAction)
         self.plotViewToolBar.addAction(self.addPolarPlotAction)
+        self.plotViewToolBar.addAction(self.addScatterPlotAction)
 
         # ADDING ALL TOOLBARS TO THE MAIN WINDOW
         self.addToolBar(self.mainToolBar)
@@ -404,6 +412,7 @@ class MainWindow(QMainWindow):
         self.icons['LINE_PLOT'] = QIcon(os.path.join(self.iconPath, 'line-plot.png'))
         self.icons['TIME_SERIES'] = QIcon(os.path.join(self.iconPath, 'time-series.png'))
         self.icons['POLAR'] = QIcon(os.path.join(self.iconPath, 'polar.png'))
+        self.icons['SCATTER'] = QIcon(os.path.join(self.iconPath, 'scatter.png'))
         self.icons['PLAY'] = QIcon(os.path.join(self.iconPath, 'play.png'))
         self.icons['PAUSE'] = QIcon(os.path.join(self.iconPath, 'pause.png'))
         self.icons['FAST_FORWARD'] = QIcon(os.path.join(self.iconPath, 'fast-forward.png'))
@@ -638,6 +647,9 @@ class MainWindow(QMainWindow):
 
     def _addPolarPlot(self):
         self.centralViewWidget.addPolarPlot()
+
+    def _addScatterPlot(self):
+        self.centralViewWidget.addScatterPlot()
 
     def _onObjectViewConfigChanged(self, noradIndex, newConfiguration):
         self.settings['VIEW_CONFIG']['OBJECTS'][str(noradIndex)] = newConfiguration
@@ -973,6 +985,8 @@ class CentralViewWidget(QWidget):
                     self.addTimeSeriesPlot(configuration=dockWidgetConfiguration["CONFIGURATION"], title=title, area=area)
                 if dockWidgetConfiguration["PLOT_TYPE"] == "POLAR":
                     self.addPolarPlot(configuration=dockWidgetConfiguration["CONFIGURATION"], title=title, area=area)
+                if dockWidgetConfiguration["PLOT_TYPE"] == "SCATTER":
+                    self.addScatterPlot(configuration=dockWidgetConfiguration["CONFIGURATION"], title=title, area=area)
 
     def addLinePlot(self, configuration=None, title=None, area=None):
         linePlot = LinePlot(self, variableRegistry=self.variableRegistry)
@@ -1009,6 +1023,18 @@ class CentralViewWidget(QWidget):
         if configuration is not None:
             polarPlot.setConfiguration(configuration)
         self.plotViewWidget.addNewPlot(widget=polarPlot, title=title, area=area)
+
+    def addScatterPlot(self, configuration=None, title=None, area=None):
+        scatterPlot = ScatterPlot(self, variableRegistry=self.variableRegistry)
+        scatterPlot.requestIndexProvider = self.generateRequestIndex
+        scatterPlot.dataRequestCreated.connect(self._onPlotDataRequestCreated)
+        scatterPlot.dataRequestUpdated.connect(self._onPlotDataRequestUpdated)
+        scatterPlot.dataRequestDestroyed.connect(self._onPlotDataRequestDestroyed)
+        self.activeObjectsChanged.connect(scatterPlot.setActiveObjects)
+        scatterPlot.setActiveObjects(self.activeObjects)
+        if configuration is not None:
+            scatterPlot.setConfiguration(configuration)
+        self.plotViewWidget.addNewPlot(widget=scatterPlot, title=title, area=area)
 
     def closeEvent(self, event):
         self.orbitWorker.stop()
--- src/gui/plots/general.py
+++ src/gui/plots/general.py
@@ -8,6 +8,7 @@ from PyQt5.QtGui import QIcon
 from src.gui.plots.line import LinePlot, LinePlotSettingsWidget
 from src.gui.plots.time import TimeSeriesPlot, TimeSeriesSettingsWidget
 from src.gui.plots.polar import PolarPlot, PolarPlotSettingsWidget
+from src.gui.plots.scatter import ScatterPlot, ScatterPlotSettingsWidget
 from src.gui.common import SquareIconButton, AreaCycler
 
 
@@ -117,6 +118,8 @@ class PlotViewTabWidget(QMainWindow):
                     plotType = "TIME_SERIES"
                 elif isinstance(dock.plotWidget, PolarPlot):
                     plotType = "POLAR"
+                elif isinstance(dock.plotWidget, ScatterPlot):
+                    plotType = "SCATTER"
                 else:
                     plotType = "NONE"
                 dockWidgets.append({"TITLE": dock.windowTitle(), "AREA": tab.dockWidgetArea(dock), "PLOT_TYPE": plotType, "CONFIGURATION": config})
@@ -180,7 +183,9 @@ class PlotSettingsDockWidget(QDockWidget):
 
     def addSettingsTab(self, title, dockWidget: PlotDockWidget):
         if dockWidget not in self.dockToSettings:
-            if isinstance(dockWidget.plotWidget, LinePlot):
+            if isinstance(dockWidget.plotWidget, ScatterPlot):
+                widget = ScatterPlotSettingsWidget(dockWidget.plotWidget, dockWidget=dockWidget, parent=self.tabWidget)
+            elif isinstance(dockWidget.plotWidget, LinePlot):
                 widget = LinePlotSettingsWidget(dockWidget.plotWidget, dockWidget=dockWidget, parent=self.tabWidget)
             elif isinstance(dockWidget.plotWidget, TimeSeriesPlot):
                 widget = TimeSeriesSettingsWidget(dockWidget.plotWidget, dockWidget=dockWidget, parent=self.tabWidget)
